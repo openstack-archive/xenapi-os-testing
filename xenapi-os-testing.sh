90a8d77066794564c540251579ddd5a274e47608
Mate Lakat <mate.lakat@citrix.com>
2014-01-14 16:32:38 +0000

    Initial commit to start an instance

---
+++ xenapi-os-testing.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+set -eux
+
+XENSERVER_PASSWORD="password"
+APPLIANCE_URL="http://downloads.vmd.citrix.com/OpenStack/xenapi-in-the-cloud-appliances/master.xva"
+
+# Download dependencies
+
+for dep in openstack-xenapi-testing-xva remote-bash; do
+    if [ -e $dep ]; then
+        ( cd $dep; git pull; )
+    else
+        git clone https://github.com/citrix-openstack/$dep
+    fi
+
+    if [ -e "$dep/bin" ]; then
+        export PATH=$PATH:$(pwd)/$dep/bin
+    fi
+done
+
+cd openstack-xenapi-testing-xva
+
+nova boot \
+    --poll \
+    --image "62df001e-87ee-407c-b042-6f4e13f5d7e1" \
+    --flavor "performance1-8" \
+    --key-name matekey instance
+
+IP=$(./get-ip-address-of-instance.sh instance)
+
+SSH_PARAMS="-i matekey.pem -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null"
+
+ssh \
+    $SSH_PARAMS \
+    root@$IP mkdir -p /opt/xenapi-in-the-cloud
+
+scp \
+    $SSH_PARAMS \
+    xenapi-in-rs.sh root@$IP:/opt/xenapi-in-the-cloud/
+
+ssh \
+    $SSH_PARAMS \
+    root@$IP bash /opt/xenapi-in-the-cloud/xenapi-in-rs.sh $XENSERVER_PASSWORD $APPLIANCE_URL
+
+./wait-until-done.sh $IP matekey.priv

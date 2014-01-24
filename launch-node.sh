3b0126cbf7452d05d16fc277e145c79f6f16c43e
Mate Lakat <mate.lakat@citrix.com>
2014-01-24 12:25:46 +0000

    Install ethtool

--- launch-node.sh
+++ launch-node.sh
@@ -58,6 +58,7 @@ sed -ie "s/'phy'/'aio'/g" /opt/xensource/sm/ISCSISR.py
 SRHACK
 
 # This is important, otherwise dhcp client will fail
+sudo apt-get -qy install ethtool
 for dev in eth0 eth1 eth2 eth3 eth4; do
     sudo ethtool -K \$dev tx off
 done
